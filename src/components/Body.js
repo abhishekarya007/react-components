ac26c2d04d90a4d465f730e4b644143494c06e3a
abhishekarya007 <abhishek.kumarst@gmail.com>
2024-07-21 17:28:14 +0530

    basic structure

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,24 @@
+import { useNavigate } from "react-router-dom";
+
 const Body = () => {
+  const buttonCss = "w-1/2 px-6 py-2 bg-blue-500 bg-opacity-70 rounded-lg";
+
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <button>File Explorer</button>
+    <div className="flex flex-col gap-5 text-2xl justify-center items-center h-screen">
+      <div className="font-bold">React components</div>
+      <div className="flex flex-col gap-5 border-2 justify-center items-center border-black h-1/2 w-1/2 p-5 rounded-lg">
+        <button
+          onClick={() => {
+            navigate("/file-explorer");
+          }}
+          className={buttonCss}
+        >
+          File Explorer
+        </button>
+        <button className={buttonCss}>Progress Bar</button>
+      </div>
     </div>
   );
 };
